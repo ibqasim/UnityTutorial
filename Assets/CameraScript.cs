658408322e04701a2b9b6526da2b8774921d3a0e
Ibrahim Qasim <ib.qasim@gmail.com>
2018-11-07 17:13:58 +0000

    Added files.

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform target;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+				// Move it backwards 5m and up 1m
+        transform.position = target.position - target.forward * 5f + target.up * 1.5f;
+        transform.rotation = Quaternion.Euler(0f, target.rotation.eulerAngles.y, 0f);
+        
+    }
+}
---
+++ Assets/MoveScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// Remember to inherit from Network Behavior if this is a networked object
+public class MoveScript : NetworkBehaviour {
+
+    // Reference to the rigidbody component
+    public Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("hello");
+    }
+
+    // Called when the local player is started
+	public override void OnStartLocalPlayer() {
+        Camera.main.GetComponent<CameraScript>().target = transform;
+        // GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    // void Update () {
+    //     float x = Input.GetAxis("Horizontal");
+    //     float y = Input.GetAxis("Vertical");
+
+    //     float rx = Input.GetAxis("Mouse X");
+    //     float ry = Input.GetAxis("Mouse Y");
+
+
+    //     transform.Translate(x, 0f, y);
+    //     transform.Rotate(0f, rx, 0f);
+    // }
+
+    void Update () {
+
+		if (!isLocalPlayer) {
+            return;
+        }
+
+        float x = Input.GetAxis("Horizontal") * 0.5f;
+        float y = Input.GetAxis("Vertical") * 0.5f;
+
+        float rx = Input.GetAxis("Mouse X");
+        float ry = Input.GetAxis("Mouse Y");
+
+
+
+        transform.Translate(x, 0f, y);
+        // rigidBody.AddForce(x, 0f, y, ForceMode.Acceleration);
+        // rigidBody.rotation = Quaternion.Euler(0f, 0f, 0f);
+
+        transform.Rotate(0f, rx, 0f);
+    }
+}
